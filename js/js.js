afd75987ff36fb81d1953b0afef9fa54830b5548
Luiz Carlos Pinheiro Junior <frickajr@yahoo.com.br>
2015-05-22 11:20:44 -0300

    Ajuste de libs e outros

---
+++ js/js.js
@@ -0,0 +1,37 @@
+//scrollspy
+
+$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+});
+
+
+//maps
+function initialize() {
+        var myLatLng = new google.maps.LatLng (-22.817104,-47.069731,17);
+        var mapOptions = {
+          center: myLatLng,
+          zoom: 15
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            animation: google.maps.Animation.DROP,
+        });
+          setInterval(function(){
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+          }, 1000);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
