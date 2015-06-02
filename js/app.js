1169816c8aca510278c6dd9d09f2b2be71bb906a
Rafael Oliveira <rafael.coelho@gmail.com>
2015-06-01 21:36:01 -0300

    removendo comentário atoa

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $('.btn-next').on('click', function(e) {
     e.preventDefault();
-    // var tabId = $($($(this).parents('.tab-pane')[0]).siblings()[0]).attr('aria-labelledby')
     var tabId = $(this).attr('href')
     $('#'+tabId+'-tab').tab('show')
   })
